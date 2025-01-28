603e3db316b33717fc31c352b79734dd84cf2d70
rihtrazh <132520216+rihtrazh@users.noreply.github.com>
2025-01-29 03:41:57 +0500

    Update server.py

--- server.py
+++ server.py
@@ -1,20 +1,40 @@
 import socket
 
-sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
+def start_server(host='127.0.0.1', port=65432):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((host, port))
+    print("Сервер запущен по адресу", host, "на порту", port)
 
-msg = ''
+    server_socket.listen(5)
+    print("Сервер слушает порт", port)
 
-while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
+    try:
+        while True:
+            client_socket, client_address = server_socket.accept()
+            print("Клиент подключился", client_address)
 
-print(msg)
+            try:
+                while True:
+                    data = client_socket.recv(1024)
+                    if not data:
+                        print("Клиент отключился", client_address)
+                        break
 
-conn.close()
+                    print("Данные получены от клиента", data.decode())
+
+                    client_socket.sendall(data)
+                    print("Данные отправлены клиенту", data.decode())
+
+            except Exception as e:
+                print("Произошла ошибка с клиентом", e)
+            finally:
+                client_socket.close()
+                print("Соединение с клиентом закрыто", client_address)
+
+    except KeyboardInterrupt:
+        print("\nСервер остановлен вручную")
+    finally:
+        server_socket.close()
+        print("Сервер закрыт")
+
+start_server()
